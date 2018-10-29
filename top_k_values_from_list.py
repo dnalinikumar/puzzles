e1778d2d513b7918efb7b6585302ee30deb6d517
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-10-28 22:02:10 -0700

    initial commit.

---
+++ top_k_values_from_list.py
@@ -0,0 +1,28 @@
+#! /usr/local/bin/python
+
+'''
+Given a list of numbers, find top k maximum values.
+Return the input list if the list has smaller number of elements.
+'''
+def top_k_max_values_in_a_list(myList, k):
+
+    if (k > len(myList)):
+        return myList
+
+    # copy first items of source to target
+    top_k_max_list = myList[0:k]
+
+    # check for the rest of the elements
+    for i in myList[k:]:
+        index, value = min(enumerate(top_k_max_list), key=lambda x: x[1])
+
+        if (i > value):
+            top_k_max_list[index] = i
+
+    return top_k_max_list
+
+
+# Test list 1
+print (top_k_max_values_in_a_list([3, 0, -3, 5, -5, 23, 2455, 2, 99, 1020, -222, 8], 5))
+
+# outputs the list: [1020, 99, 2455, 8, 23]
