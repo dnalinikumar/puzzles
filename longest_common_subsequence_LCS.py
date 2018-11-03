bdc8319faf083bd1a1c70bdf0918d578bab62bdb
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-11-03 11:18:22 -0700

    only LCS length.

--- longest_common_subsequence_LCS.py
+++ longest_common_subsequence_LCS.py
@@ -19,37 +19,11 @@ def LCS(X, Y, lenX, lenY):
         return max(LCS(X, Y, lenX-1, lenY), LCS(X, Y, lenX, lenY-1))
 
 
-
-
-
-'''
-Given two strings, find a longest common subsequence
-Example: X = "ABCBDAB", Y="BDCABA"
-"BCBA", "BCAB", "BDAB" are longest sequences.
-'''
-def LCS_string(X, Y, lenX, lenY):
-
-    # return if we have reached the end of either sequence
-    if lenX == 0 or lenY == 0:
-        return ''
-
-    # if last character of X and Y matches
-    if X[lenX-1] == Y[lenY-1]:
-        return LCS_string(X, Y, lenX-1, lenY-1) + X[lenX-1]
-    else:
-        return max(LCS_string(X, Y, lenX-1, lenY), LCS_string(X, Y, lenX, lenY-1))
-
-
-
 # Test the results
 
 x = 'ABCBDAB'
 y = 'BDCABA'
 res = LCS(x, y, len(x), len(y))
-print(res)
-
-
-res = LCS_string(x, y, len(x), len(y))
 print(res)
 
 
