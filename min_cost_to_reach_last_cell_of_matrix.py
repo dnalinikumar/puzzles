bd946c6da120163d8b70bac47015740a0d069ba9
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-11-03 18:39:58 -0700

    added memoization

--- min_cost_to_reach_last_cell_of_matrix.py
+++ min_cost_to_reach_last_cell_of_matrix.py
@@ -7,7 +7,7 @@ http://www.techiedelight.com/find-minimum-cost-reach-last-cell-matrix-first-cell
 
 import sys
 
-def min_cost(matrix, m, n):
+def min_cost(matrix, m, n, memo):
     '''
     Given a M x N matrix, where each cell has a cost associated with it,
     find the minimum cost to reach last cell (M-1, N-1) of the matrix from its
@@ -19,10 +19,14 @@ def min_cost(matrix, m, n):
     if m == 1 and n == 1:
         return matrix[0][0]
     else:
-        return matrix[m-1][n-1] + min(
-            min_cost(matrix, m, n - 1),
-            min_cost(matrix, m - 1, n)
-        )
+        if (m, n-1) not in memo.keys():
+            memo[(m, n-1)] = min_cost(matrix, m, n - 1, memo)
+
+        if (m-1, n) not in memo.keys():
+            memo[(m-1, n)] = min_cost(matrix, m-1, n, memo)
+
+
+        return min(memo[(m, n-1)], memo[(m-1, n)] + matrix[m-1][n-1])
 
 
 # Test
@@ -34,4 +38,6 @@ matrix = [
         [2, 9, 8, 9, 3]
     ]
 
-print(min_cost(matrix, len(matrix), len(matrix[0])))
+memo = {}
+print(min_cost(matrix, len(matrix), len(matrix[0]), memo))
+print(memo)
