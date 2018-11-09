775e3c8c1b51ae012a307dd91eef9a7e384bda2b
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-11-09 14:03:48 -0800

    single linked list

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -2,8 +2,7 @@
 ''' tested with Python 3.7.0 '''
 
 '''
-Given a list of numbers, find top k maximum values.
-Return the input list if the list has smaller number of elements.
+Singly linked list operations.
 '''
 
 class Node:
@@ -12,31 +11,105 @@ class Node:
         self.next = None
 
 class SinglyLinkedList:
+
     def __init__(self):
-        self.head = None
+        '''constructor'''
+        self.__head = None
 
-    def addNode(self, node):
-        node.next = self.head
-        self.head = node
+    def printData(self):
+        '''Prints all the data in the list'''
+        iter = self.__head
+        while iter is not None:
+            print(iter.data)
+            iter = iter.next
 
-    def deleteNode(self, node):
-        self.head = node.next
-        return node
+    def printDataReverse(self):
+        '''Prints all the data in the list'''
+        def getData(iter):
+            if iter is not None:
+                getData(iter.next)
+                print(iter.data)
 
-    def findLoop(self):
-        if self.head != None:
-            iter1 = self.head
-            iter2 = self.head
+        getData(self.__head)
 
-            while iter2 != None or iter1 != None:
-                iter1 = iter1.next
-                iter2 = iter2.next
+    def addNode(self, data):
+        '''Adds a new node at the head'''
+        node = Node(data)
+        if self.__head is None:
+            self.__head = node
+        else:
+            node.next = self.__head
+            self.__head = node
 
-                if iter2.next is not None:
-                    iter2 = iter2.next
+    def deleteHeadNode(self):
+        '''Deletes a node at the Head'''
+        data = None
+        if self.__head is not None:
+            data = self.__head.data
+            self.__head = self.__head.next
 
-                if iter1 == iter2:
-                    return True
+        return data
 
-        return False
+    def deleteNode(self, data, all=False):
+        '''
+        Deletes a node(s) based on the data.
+        if all=True, then all the nodes matching the data would be deleted.
+        '''
+        prev = iter = self.__head
+        while iter is not None:
+            if iter.data == data:
+                if iter == self.__head:
+                    self.__head = iter.next
+                else:
+                    prev.next = iter.next
+
+                # Should all notes be deleted or no?
+                if all == False:
+                    break
+
+            prev = iter
+            iter = iter.next
+
+    def traverse(self):
+        '''Prints all the link information in the list'''
+        iter = self.__head
+        count = 1
+        while iter is not None:
+            print(f"{count}, self: {iter}, next: {iter.next}, data: {iter.data}")
+            iter = iter.next
+            count += 1
+
+d = SinglyLinkedList()
+
+# Add new nodes at the tail with some duplicate data
+d.addNode(5)
+d.addNode(6)
+d.addNode(7)
+d.addNode(8)
+d.addNode(9)
+d.addNode(9)
+d.addNode(9)
+d.addNode(9)
+d.addNode(10)
+d.printData()
+print("added new nodes")
+
+print(d.deleteHeadNode())
+d.printData()
+print("Deleted Head node")
+
+d.deleteNode(9)
+d.printData()
+print("Deleted one node with some data")
+
+d.deleteNode(9, all=True)
+d.printData()
+print("Deleted all nodes with same data")
+
+d.traverse()
+print("traverse through all the links")
+
+d.printDataReverse()
+print("print data in reverse\n")
+
 
