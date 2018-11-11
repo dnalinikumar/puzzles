c3dc3de0ee086d542a2d14463b8a47e99d597a06
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-11-10 23:14:18 -0800

    added response with multiple palindromes

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -5,23 +5,24 @@ def longest_palindrome_substring(s):
     ''' brute force algorithm'''
     n = len(s)
     max_len = 1
-    palindrome = ''
+    palindrome = []
 
-    for j in range (1, n):
-        for i in range(n-j):
+    for j in range (n, 1, -1):
+        for i in range(n - j):
             x = s[i: i+j+1]
+
+            print((f'x={x}'))
             xLen = len(x)
 
             for k in range(xLen//2):
                 if (x[k] != x[xLen-1-k]):
                     break
             else:
-                if len(x) > max_len:
+                if len(x) >= max_len:
                     max_len = len(x)
-                    palindrome = x
+                    palindrome.append(x)
 
     return max_len, palindrome
 
-
-s = "pwexxxkxxxewxyz"
+s = "pttwekewxyqyx"
 print(longest_palindrome_substring(s))
