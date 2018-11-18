b257880c6a6a00084f8cc414e1e099000697ecb0
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-11-17 21:32:53 -0800

    working Binary Search tree operations

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -14,70 +14,199 @@ class BinarySearchTree:
 
     def min(self):
         iter = self.root
-        while iter.left:
+        while iter and iter.left:
             iter = iter.left
-        return iter.data
+        return iter and iter.data
 
     def max(self):
         iter = self.root
-        while iter.right:
+        while iter and iter.right:
             iter = iter.right
-        return iter.data
+        return iter and iter.data
 
     def insert(self, data):
         '''insert a new node into the tree.'''
         if self.root is None:
             self.root = Node(data)
-            self.left = None
-            self.right = None
-            return
-
-        def insertNode(node, data):
-            if data < node.data:
-                if node.left is None:
-                    node.left = Node(data)
-                else:
-                    insertNode(node.left, data)
-            else:
-                if node.right is None:
-                    node.right = Node(data)
-                else:
-                    insertNode(node.right, data)
-
-        insertNode(self.root, data)
-
-    def inorderTraversal(self, data):
-        '''insert a new node into the tree.'''
-        if self.root is None:
-            self.root = Node(data)
-            self.left = None
-            self.right = None
-            return
-
-        def insertNode(node, data):
-            if data < node.data:
-                if node.left is None:
-                    node.left = Node(data)
-                else:
-                    insertNode(node.left, data)
-            else:
-                if node.right is None:
-                    node.right = Node(data)
-                else:
-                    insertNode(node.right, data)
-
-        insertNode(self.root, data)
-
-    # def delete(self, data):
-
-
-
-    def is_mirror(t1, t2):
-        if t1 is None and t2 is None:
+            self.size = 1
             return True
+
+        def insertNode(node, data):
+            if data < node.data:
+                if node.left is None:
+                    node.left = Node(data)
+                else:
+                    return insertNode(node.left, data)
+            elif data > node.data:
+                if node.right is None:
+                    node.right = Node(data)
+                else:
+                    return insertNode(node.right, data)
+            else:
+                raise ValueError
+
+        try:
+            insertNode(self.root, data)
+        except ValueError:
+            return False
         else:
-            return t1 is not None and \
-                t2 is not None and \
-                t1.data == t2.data and \
-                is_mirror(t1.left, t2.right) and \
-                is_mirror(t1.right, t2.left)
+            self.size += 1
+            return True
+
+
+    def delete(self, data):
+        '''delete a node from the tree.'''
+        if self.root is None:                   # no nodes in the tree
+            return False
+        elif self.root.data == data:            # root node to be deleted
+            if self.root.left is None:          # root node to be deleted with no left node.
+                self.root = self.root.right
+            elif self.root.right is None:       # root node to be deleted with no right node.
+                self.root = self.root.left
+            else:                               # root node to be deleted, but left and right nodes present.
+                prev = iter = self.root.left
+                while iter and iter.right:      # go till the right most node on the left sub tree.
+                    prev = iter
+                    iter = iter.right
+
+                if iter != self.root.left:      # If right nodes are present on the left subtree
+                    prev.right = iter.left
+
+                iter.right = self.root.right
+                self.root = iter
+
+            self.size -= 1
+            return True
+        else:                                   # non root node to be found, deleted
+            prev = iter = self.root
+            while iter:
+                if data < iter.data:            # go to the left subtree
+                    prev = iter
+                    iter = iter.left
+                elif data > iter.data:          # go to the right subtree
+                    prev = iter
+                    iter = iter.right
+                else:                           # node found.
+                    break
+
+            if iter is None:                    # Node with the data not found.
+                return False
+
+            if iter.left is None:               # No left subtree from the node found.
+                if prev.left == iter:
+                    prev.left = iter.right
+                elif prev.right == iter:
+                    prev.right = iter.right
+            elif iter.right is None:            # No right subtree from the node found.
+                if prev.left == iter:
+                    prev.left = iter.left
+                elif prev.right == iter:
+                    prev.right = iter.left
+            else:                               # have both right children
+                if prev.left == iter:
+                    prev1 = iter = iter.right
+                    while iter.left:
+                        prev1 = iter
+                        iter = iter.left
+
+                    if prev1 == iter:           # no further left subtree
+                        prev1.left = prev.left.left
+                        prev.left = prev1
+                    else:
+                        prev1.left = iter.right
+                        iter.right = prev.left.right
+                        iter.left = prev.left.left
+                        prev.left = iter
+
+                elif prev.right == iter:
+                    prev1 = iter = iter.left
+                    while iter.right:
+                        prev1 = iter
+                        iter = iter.right
+
+                    if prev1 == iter:            # no further right subtree
+                        prev1.right = prev.right.right
+                        prev.right = prev1
+                    else:
+                        prev1.left = iter.right
+                        iter.right = prev.right.right
+                        iter.left = prev.right.left
+                        prev.right = iter
+
+            return True
+
+    def inorderTraversal(self):
+        '''Inorder traversal.'''
+        def inorder(node):
+            if node is not None:
+                inorder(node.left)
+                print(' ', node.data, end='')
+                inorder(node.right)
+
+        inorder(self.root)
+
+    def preorderTraversal(self):
+        '''Inorder traversal.'''
+        def preorder(node):
+            if node is not None:
+                print(' ', node.data, end='')
+                preorder(node.left)
+                preorder(node.right)
+
+        preorder(self.root)
+
+    def postorderTraversal(self):
+        '''Inorder traversal.'''
+        def postorder(node):
+            if node is not None:
+                postorder(node.right)
+                postorder(node.left)
+                print(' ', node.data, end='')
+
+        postorder(self.root)
+
+if __name__ == '__main__':
+    # Create a binary search tree
+    t = BinarySearchTree()
+
+    # Insert elements
+    t.insert(3)
+    t.insert(5)
+    t.insert(7)
+    t.insert(2)
+    t.insert(9)
+    t.insert(0)
+    t.insert(-3)
+    t.insert(1)
+    t.insert(200)
+    t.insert(-9)
+
+    # inorder traversal
+    print('\nInorder traversal:')
+    t.inorderTraversal()
+
+    # preorder traversal
+    print('\nPreorder traversal:')
+    t.preorderTraversal()
+
+    # postorder traversal
+    print('\nPostorder traversal:')
+    t.postorderTraversal()
+
+    print('\n')
+
+    # delete an element.
+    t.delete(2)
+
+    # inorder traversal
+    print('\nInorder traversal:')
+    t.inorderTraversal()
+
+    # preorder traversal
+    print('\nPreorder traversal:')
+    t.preorderTraversal()
+
+    # postorder traversal
+    print('\nPostorder traversal:')
+    t.postorderTraversal()
+    print('\n')
