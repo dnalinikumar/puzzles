0059d328b4e591e003daee9c1d0142d90bb6c32b
Nalini Kumar Danda <nalini_danda@cable.comcast.com>
2018-11-18 14:09:22 -0800

    Leet code 189: rotate array

---
+++ lt189_rotate_array.py
@@ -0,0 +1,34 @@
+#! /usr/local/bin/python
+''' tested with Python 3.7.0 '''
+
+'''
+Leet code 189:
+Given an array, rotate the array to the right by k steps, where k 
+is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+'''
+
+def rotate_array(l, k):
+
+
+
+if __name__ == '__main__':
+    print(rotate_array([1,2,3,4,5,6,7], 3))
+
+    print(rotate_array([-1,-100,3,99], 2))
